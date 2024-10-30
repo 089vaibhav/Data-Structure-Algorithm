85ae0b7d5f52e3f92424f0030de30c545d3b867e
Vaibhav <1722vaibhav.sharma@gmail.com>
2024-10-30 15:16:24 +0530

    Binary Search using Recursion

--- recursion/binarySearch.cpp
+++ recursion/binarySearch.cpp
@@ -1,33 +1,36 @@
-#include<iostream>
+#include<iostream> // Include the iostream library for input and output
 using namespace std;
 
-bool isSorted(int arr[],int size){
-
-    if(size==0 || size==1){
+// Function to check if an array is sorted in ascending order
+bool isSorted(int arr[], int size) {
+    // Base case: if the array has 0 or 1 element, it's considered sorted
+    if (size == 0 || size == 1) {
         return true; 
     }
 
-    if(arr[0]>arr[1]){
+    // Check if the first element is greater than the second; if so, array is not sorted
+    if (arr[0] > arr[1]) {
         return false;
+    } else {
+        // Recursively check the rest of the array
+        bool remainingArr = isSorted(arr + 1, size - 1);
+        return remainingArr;
     }
-    else{
-        bool remainingArr = isSorted(arr+1, size-1);
-        return remainingArr ;
-    }
-
 }
 
-int main(){
-    int arr[10] = {2,4,5,6,7,88,9,33,44,55};
-    int size = 10;
+int main() {
+    int arr[10] = {2, 4, 5, 6, 7, 88, 9, 33, 44, 55}; // Define an array of 10 integers
+    int size = 10; // Size of the array
+
+    // Check if the array is sorted and store the result in 'ans'
     bool ans = isSorted(arr, size);
 
-    if(ans){
-        cout<<"Array is sorted"<<endl;
-    }
-    else{
-        cout<<"Array is not sorted"<<endl;
+    // Output the result based on the value of 'ans'
+    if (ans) {
+        cout << "Array is sorted" << endl;
+    } else {
+        cout << "Array is not sorted" << endl;
     }
 
-    return 0;
+    return 0; // End of program
 }
